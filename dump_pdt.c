afabdf7d0e263886880418a19a00bd3bf4d91c17
Yoshimaster96 <yoshimaster96smwc@gmail.com>
2022-04-17 20:37:31 -0500

    Properly handle channel count for .pdt files
    Properly handle channel count for .pdt files (are mono streams even used anywhere? added just in case)

--- dump_pdt.c
+++ dump_pdt.c
@@ -43,9 +43,9 @@ int main(int argc, char ** argv) {
 		uint16_t unk116		= read_u16_be(fp);
 		uint32_t ch1CoefOffs	= coeffOffs+(ch1CoefEntry<<5);
 		
-		uint32_t ch2Start	= 0;
-		uint16_t ch2CoefEntry	= 0;
-		uint32_t ch2CoefOffs	= 0;
+		uint32_t ch2Start	= ch1Start;
+		uint16_t ch2CoefEntry	= ch1CoefEntry;
+		uint32_t ch2CoefOffs	= coeffOffs+(ch2CoefEntry<<5);
 		int chanCount = 1;
 		
 		if(flags&0x01000000) {
@@ -61,81 +61,123 @@ int main(int argc, char ** argv) {
 		
 		char fname[0x100];
 		
-		//Write left channel
-		snprintf(fname,0x100,"%04X_L.dsp",i);
-		FILE * out = fopen(fname,"wb");
-		
-		write_u32_be(out,nibblesToSamples(nibbleCount));
-		write_u32_be(out,nibbleCount);
-		write_u32_be(out,sampleRate);
-		write_u16_be(out,loopFlag);
-		write_u16_be(out,0);
-		write_u32_be(out,loopStart);
-		write_u32_be(out,nibbleCount-1);
-		write_u32_be(out,0);
-		
-		fseek(fp,ch1CoefOffs,SEEK_SET);
-		for(int j=0; j<16; j++) {
-			write_u16_be(out,read_u16_be(fp));
-		}
-		
-		write_u16_be(out,0);
-		fseek(fp,ch1Start,SEEK_SET);
-		write_u16_be(out,read_u8_be(fp));
-		write_u16_be(out,0);
-		write_u16_be(out,0);
-		write_u16_be(out,0);
-		write_u16_be(out,0);
-		write_u16_be(out,0);
-		
-		for(int j=0; j<11; j++) {
+		if(chanCount==1) {
+			//Write mono channel
+			snprintf(fname,0x100,"%04X.dsp",i);
+			FILE * out = fopen(fname,"wb");
+			
+			write_u32_be(out,nibblesToSamples(nibbleCount));
+			write_u32_be(out,nibbleCount);
+			write_u32_be(out,sampleRate);
+			write_u16_be(out,loopFlag);
 			write_u16_be(out,0);
-		}
-		
-		fseek(fp,ch1Start,SEEK_SET);
-		for(int j=0; j<(nibbleCount<<1); j++) {
-			putc(getc(fp),out);
-		}
-		
-		fclose(out);
-		
-		//Write right channel
-		snprintf(fname,0x100,"%04X_R.dsp",i);
-		out = fopen(fname,"wb");
-		
-		write_u32_be(out,nibblesToSamples(nibbleCount));
-		write_u32_be(out,nibbleCount);
-		write_u32_be(out,sampleRate);
-		write_u16_be(out,loopFlag);
-		write_u16_be(out,0);
-		write_u32_be(out,loopStart);
-		write_u32_be(out,nibbleCount-1);
-		write_u32_be(out,0);
-		
-		fseek(fp,ch2CoefOffs,SEEK_SET);
-		for(int j=0; j<16; j++) {
-			write_u16_be(out,read_u16_be(fp));
-		}
-		
-		write_u16_be(out,0);
-		fseek(fp,ch2Start,SEEK_SET);
-		write_u16_be(out,read_u8_be(fp));
-		write_u16_be(out,0);
-		write_u16_be(out,0);
-		write_u16_be(out,0);
-		write_u16_be(out,0);
-		write_u16_be(out,0);
-		
-		for(int j=0; j<11; j++) {
+			write_u32_be(out,loopStart);
+			write_u32_be(out,nibbleCount-1);
+			write_u32_be(out,0);
+			
+			fseek(fp,ch1CoefOffs,SEEK_SET);
+			for(int j=0; j<16; j++) {
+				write_u16_be(out,read_u16_be(fp));
+			}
+			
 			write_u16_be(out,0);
+			fseek(fp,ch1Start,SEEK_SET);
+			write_u16_be(out,read_u8_be(fp));
+			write_u16_be(out,0);
+			write_u16_be(out,0);
+			write_u16_be(out,0);
+			write_u16_be(out,0);
+			write_u16_be(out,0);
+			
+			for(int j=0; j<11; j++) {
+				write_u16_be(out,0);
+			}
+			
+			fseek(fp,ch1Start,SEEK_SET);
+			for(int j=0; j<(nibbleCount<<1); j++) {
+				putc(getc(fp),out);
+			}
+			
+			fclose(out);
 		}
 		
-		fseek(fp,ch2Start,SEEK_SET);
-		for(int j=0; j<(nibbleCount<<1); j++) {
-			putc(getc(fp),out);
+		if(chanCount==2) {
+			//Write left channel
+			snprintf(fname,0x100,"%04X_L.dsp",i);
+			FILE * out = fopen(fname,"wb");
+			
+			write_u32_be(out,nibblesToSamples(nibbleCount));
+			write_u32_be(out,nibbleCount);
+			write_u32_be(out,sampleRate);
+			write_u16_be(out,loopFlag);
+			write_u16_be(out,0);
+			write_u32_be(out,loopStart);
+			write_u32_be(out,nibbleCount-1);
+			write_u32_be(out,0);
+			
+			fseek(fp,ch1CoefOffs,SEEK_SET);
+			for(int j=0; j<16; j++) {
+				write_u16_be(out,read_u16_be(fp));
+			}
+			
+			write_u16_be(out,0);
+			fseek(fp,ch1Start,SEEK_SET);
+			write_u16_be(out,read_u8_be(fp));
+			write_u16_be(out,0);
+			write_u16_be(out,0);
+			write_u16_be(out,0);
+			write_u16_be(out,0);
+			write_u16_be(out,0);
+			
+			for(int j=0; j<11; j++) {
+				write_u16_be(out,0);
+			}
+			
+			fseek(fp,ch1Start,SEEK_SET);
+			for(int j=0; j<(nibbleCount<<1); j++) {
+				putc(getc(fp),out);
+			}
+			
+			fclose(out);
+			
+			//Write right channel
+			snprintf(fname,0x100,"%04X_R.dsp",i);
+			out = fopen(fname,"wb");
+			
+			write_u32_be(out,nibblesToSamples(nibbleCount));
+			write_u32_be(out,nibbleCount);
+			write_u32_be(out,sampleRate);
+			write_u16_be(out,loopFlag);
+			write_u16_be(out,0);
+			write_u32_be(out,loopStart);
+			write_u32_be(out,nibbleCount-1);
+			write_u32_be(out,0);
+			
+			fseek(fp,ch2CoefOffs,SEEK_SET);
+			for(int j=0; j<16; j++) {
+				write_u16_be(out,read_u16_be(fp));
+			}
+			
+			write_u16_be(out,0);
+			fseek(fp,ch2Start,SEEK_SET);
+			write_u16_be(out,read_u8_be(fp));
+			write_u16_be(out,0);
+			write_u16_be(out,0);
+			write_u16_be(out,0);
+			write_u16_be(out,0);
+			write_u16_be(out,0);
+			
+			for(int j=0; j<11; j++) {
+				write_u16_be(out,0);
+			}
+			
+			fseek(fp,ch2Start,SEEK_SET);
+			for(int j=0; j<(nibbleCount<<1); j++) {
+				putc(getc(fp),out);
+			}
+			
+			fclose(out);
 		}
-		
-		fclose(out);
 	}
 	
 	fclose(fp);
